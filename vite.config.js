9c11a75693e058a189232522a268e43ddfe96200
signsofdidier <signsofdidier@gmail.com>
2025-09-17 00:59:36 +0200

    images met punt voor web

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/unwind/',
   plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
